547ba896ee7f167d900082ae121405409d53cc93
ellie fard <enesarifard@gmail.com>
2020-06-22 08:17:09 -0400

    added signin page and header

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route
           exact
@@ -21,8 +19,12 @@ function App() {
           component={HomePage}
         />
         <Route
-          path='/hats'
-          component={HatsPage}
+          path='/shop'
+          component={ShopPage}
+        />
+        <Route
+          path='/signin'
+          component={SignInAndSignUpPage}
         />
       </Switch>
     </div>
